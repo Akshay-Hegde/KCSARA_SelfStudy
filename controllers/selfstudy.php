22447ed5f3de97e54c096695da792c25e86896a8
Douglas Burchard <doug@douglasburchard.com>
2014-06-05 09:09:16 -0700

    Flushed out the front-end controller file to an early alpha state (loads model and view).

--- controllers/selfstudy.php
+++ controllers/selfstudy.php
@@ -1,1 +1,19 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class SelfStudy extends Public_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('selfstudy_m');
+
+	}
+
+	public function index()
+	{
+		$this->template
+			->title( 'Self Study' )
+        	->build('index');
+	}	
+}
