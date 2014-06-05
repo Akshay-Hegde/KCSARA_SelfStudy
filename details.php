0863bf794e22af75fac904c2d33bace3a6d76b9d
Douglas Burchard <doug@douglasburchard.com>
2014-06-05 09:54:48 -0700

    Added install and uninstall functionality to the detail file.

--- details.php
+++ details.php
@@ -21,24 +21,79 @@ class Module_SelfStudy extends Module {
 			'description' => array(
 				'en' => 'The selfstudy module creates a multi-page self study sub-site from a single HTML-formatted file stored on GitHub.',
 			),
-			'frontend' => true,
-			'backend' => true,
+			'frontend' => TRUE,
+			'backend' => TRUE,
 			'menu' => 'content'
 		);
 	}
 
 	public function install()
 	{
-		return true;
+
+		$_table_prefix = $this->config->item('selfstudy._table_prefix');
+
+		$this->dbforge->drop_table($_table_prefix . 'lessons');
+		$this->dbforge->drop_table($_table_prefix . 'courses');
+
+		$this->db->delete('settings', array('module' => 'exams'));
+
+		$tables = array(
+			$_table_prefix . 'lessons' => array(
+				'lessonid' => array('type' => 'CHAR', 'constraint' => 64, 'primary' => true),
+				'courseid' => array('type' => 'CHAR', 'constraint' => 64),
+				'slub' => array('type' => 'CHAR', 'constraint' => 64),
+				'title' => array('type' => 'VARCHAR', 'constraint' => 255, 'default' => ''),
+				'html' => array('type' => 'LONGTEXT')
+			),
+
+			$_table_prefix . 'courses' => array(
+				'courseid' => array('type' => 'CHAR', 'constraint' => 64, 'primary' => true),
+				'slub' => array('type' => 'CHAR', 'constraint' => 64),
+				'title' => array('type' => 'VARCHAR', 'constraint' => 255, 'default' => ''),
+				'description' => array('type' => 'VARCHAR', 'constraint' => 255, 'default' => ''),
+				'version' => array('type' => 'VARCHAR', 'constraint' => 255, 'default' => ''),
+				'tochtml' => array('type' => 'LONGTEXT'),
+				'rawhtml' => array('type' => 'LONGTEXT')
+			),
+
+		);
+
+
+		$module_settings = array(
+			'slug' => 'selfstudy_setting',
+			'title' => 'Self Study Setting',
+			'description' => 'A Yes or No option for the Self Study module',
+			'`default`' => '1',
+			'`value`' => '1',
+			'type' => 'select',
+			'`options`' => '1=Yes|0=No',
+			'is_required' => 1,
+			'is_gui' => 1,
+			'module' => 'selfstudy'
+		);
+
+		if( $this->install_tables($tables) AND
+		   $this->db->insert('settings', $module_settings) )
+		{
+			return TRUE;
+		}
 	}
 
 	public function uninstall()
 	{
-		return true;
+		$_table_prefix = $this->config->item('selfstudy._table_prefix');
+
+		$this->dbforge->drop_table($_table_prefix . 'lessons');
+		$this->dbforge->drop_table($_table_prefix . 'courses');
+
+		$this->db->delete('settings', array('module' => 'selfstudy'));
+		{
+			return TRUE;
+		}
 	}
 
 	public function upgrade($old_version)
 	{
-		return true;
+		return TRUE;
 	}
 }
