36a116e2aa0e881ff7864eb8be53d17010f33de0
Douglas Burchard <doug@douglasburchard.com>
2014-06-05 08:54:03 -0700

    Flushed out the details file to an alpha state.

--- details.php
+++ details.php
@@ -1,1 +1,44 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Module_SelfStudy extends Module {
+
+	public $version = 'a1.0';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->config->load('selfstudy/selfstudy');
+        $this->template->active_section = 'selfstudy';
+    }
+
+	public function info()
+	{
+		return array(
+			'name' => array(
+				'en' => 'SelfStudy',
+			),
+			'description' => array(
+				'en' => 'The selfstudy module creates a multi-page self study sub-site from a single HTML-formatted file stored on GitHub.',
+			),
+			'frontend' => true,
+			'backend' => true,
+			'menu' => 'content'
+		);
+	}
+
+	public function install()
+	{
+		return true;
+	}
+
+	public function uninstall()
+	{
+		return true;
+	}
+
+	public function upgrade($old_version)
+	{
+		return true;
+	}
+}
